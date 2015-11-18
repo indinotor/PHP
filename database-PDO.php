42f0365da26a56c91bfc52fc4f5c6efa949202eb
indinotor <indinotor@gmail.com>
2015-11-18 13:07:07 +0100

    Update database-PDO.php

--- database-PDO.php
+++ database-PDO.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-    $db = new PDO("mysql:host=localhost;dbname=potatoNetworth", "root", "");
+    $db = new PDO("mysql:host=localhost;dbname=databaseName", "root", "");
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $db->exec("SET NAMES utf8");
 } catch (PDOException $e) {
